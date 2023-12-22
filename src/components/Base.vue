f190390886cfcdb2599d5f80eb477d427e0dbee9
Oleksandr Sikorskyi <o.sikorskyi.vn@gmail.com>
2023-12-22 10:31:22 +0200

    cleaned

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -1,26 +1,19 @@
 <script setup>
 import Info from './Info.vue';
 import SqlBlock from './SqlBock.vue';
-
 </script>
 
 <template>
   <div class="content">
     <div class="content__navbar">
-      
       <Info/>
     </div>
     <div class="content__main">
-      <Info />
-      <hr>
-      <SqlBlock/>
-    <div class="content__main"> 
       <SqlBlock/>
     </div>
     <div class="content__footer">
       Footer
     </div>
-  </div>
   </div>
 </template>
 
