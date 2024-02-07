b154fcf7661f36704af11d2c2ed13ec2464ac9ec
Oleksandr Sikorskyi <o.sikorskyi.vn@gmail.com>
2024-02-07 03:54:48 +0200

    revorked buttons

--- src/components/SqlBock.vue
+++ src/components/SqlBock.vue
@@ -1,54 +1,50 @@
 <script setup>
-import { ref, computed} from 'vue';
+import { ref, computed } from 'vue';
 import { useApiFetch } from '/src/helpers/helpers.js';
-import { sql_query} from '/src/helpers/helpers.js'
+import { sql_query } from '/src/helpers/helpers.js'
 import Table from './Table.vue'
 import TabBtn from './TabBtn.vue'
 import allQueries from './../../queries.json'
 const type_query = ref('join')
 
 
-
-
-const { execute, data, error, isFetching } = useApiFetch('sql', { 
-    immediate: false })
+const { execute, data, error, isFetching } = useApiFetch('sql', {
+    immediate: false
+})
     .post(() => sql_query.s_query)
     .json()
 
 function simple_query(event) {
-    sql_query.s_query = allQueries[type_query.value][event.target.value];
+    sql_query.s_query = allQueries[type_query.value].buttons[event.target.value];
     execute();
 }
 
-function change_type(value){
+function change_type(value) {
     type_query.value = value;
-    
+
 }
 
-const selected_buttons = computed(()=> {
-    if(type_query.value){
-        return allQueries[type_query.value];
+const selected_buttons = computed(() => {
+    if (type_query.value) {
+        return allQueries[type_query.value].buttons;
     }
-    });
+});
 
 </script>
 
 <template>
     <div class="row row--tabs">
-        <TabBtn v-for="_, value in allQueries" :value="value" :actual_type="type_query" @change_type="change_type"></TabBtn>
-        <!-- <button class="tab-btn" type="button" @click="change_type" value="join" :class="isActive('join')">Прості запити</button> 
-        <button class="tab-btn" type="button" @click="change_type" value="where" :class="isActive('where')">Складна умова</button> 
-        <button class="tab-btn" type="button" @click="change_type" value="sum" :class="isActive('sum')">Підсумковий запит</button> 
-        <button class="tab-btn" type="button" @click="change_type" value="asc_desk" :class="isActive('asc_desk')">Упорядкування</button> 
-        <button class="tab-btn" type="button" @click="change_type" value="search" :class="isActive('search')">Пошук</button>  -->
+        <TabBtn v-for="_, value in allQueries" :value="value" :title=allQueries[value].title :actual_type="type_query"
+            @change_type="change_type">
+        </TabBtn>
     </div>
     <div class="row">
-        
+
         <div class="row__element">
-           
-            
             <button v-for="(_, text, index) in selected_buttons" class="simple-sql-btn" type="button" @click="simple_query"
-                :value="text" :key="index">{{ text }}</button>
+                :value="text" :key="index">
+                {{ text }}
+            </button>
         </div>
     </div>
     <hr>
@@ -57,10 +53,10 @@ const selected_buttons = computed(()=> {
             <form action="none">
                 <fieldset>
                     <legend>SQL запит</legend>
-                    <textarea class="prettyprint" ref="t_area" id="sql-input" rows="10"  v-model="sql_query.s_query"
+                    <textarea class="prettyprint" ref="t_area" id="sql-input" rows="10" v-model="sql_query.s_query"
                         placeholder="Add sql query">
                     </textarea>
-                    <br/>
+                    <br />
                     <button id="sql-btn" type="button" @click="execute">Надіслати запит</button>
                 </fieldset>
             </form>
@@ -91,6 +87,4 @@ const selected_buttons = computed(()=> {
         </div>
     </div>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/TabBtn.vue
+++ src/components/TabBtn.vue
@@ -1,11 +1,12 @@
 <script setup>
 const props = defineProps({
     value: String,
-    actual_type: String
+    actual_type: String,
+    title: String
 })
 const isActive = () => props.value == props.actual_type ? "active" : "";
 
 </script>
 <template>
-    <button class="tab-btn" type="button" @click="$emit('change_type', value)" :class="isActive()">Прості запити</button>
+    <button class="tab-btn" type="button" @click="$emit('change_type', value)" :class="isActive()">{{title}}</button>
 </template>
